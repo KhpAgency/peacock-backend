793f761db8508d1491a2d7ab67e0df87c4d04267
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-06-08 12:49:41 +0300

    last commit

--- utils/validators/categoryValidator.js
+++ utils/validators/categoryValidator.js
@@ -1,5 +1,7 @@
 const { check } = require("express-validator");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const categoriesModel = require("../../models/categoriesModel");
+
 
 exports.getCategoryValidator = [
   check("id").isMongoId().withMessage("Invalid category id format"),
@@ -11,7 +13,15 @@ exports.createCategoryvalidator = [
     .notEmpty()
     .withMessage("Category name is required")
     .isLength({ min: 4 })
-    .withMessage("too short category name"),
+    .withMessage("too short category name")
+    .custom(async (value) => {
+      let category = await categoriesModel.find({ name: value });
+      if (category.length > 0) {
+        throw new Error(
+          `Category name: ( ${value} ) already exists! Choose another name!`
+        );
+      }
+    }),
   validatorMiddleware,
 ];
 
