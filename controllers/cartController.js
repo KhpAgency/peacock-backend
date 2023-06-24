8ffbc66eadfd0710d997d414af9ea8c415716de6
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-06-24 15:40:07 +0300

    latest commit

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -2,17 +2,15 @@ const asyncHandler = require("express-async-handler");
 const ApiError = require("../utils/ApiError");
 const cartModel = require("../models/cartModel");
 
-
 exports.addProductToCart = asyncHandler(async (req, res, next) => {
-    // Get cart for logged in user
-    let cart = await cartModel.findOne({user: req.user._id});
-    if (!cart) {
-        // create a new cart for the logged in user
-        const{product, variant, quantity, price} = req.body
-        cart = await cartModel.create({
-            user: req.user._id,
-            cartItems:[{product, variant, quantity, price}]
-        })
-    }
-})
-
+  // Get cart for logged in user
+  let cart = await cartModel.findOne({ user: req.user._id });
+  if (!cart) {
+    // create a new cart for the logged in user
+    const { productCategory, productID, variant, price } = req.body;
+    cart = await cartModel.create({
+      user: req.user._id,
+      cartItems: [{ productCategory, productID, variant, price }],
+    });
+  }
+});
