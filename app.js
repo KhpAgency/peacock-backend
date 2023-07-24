f468b80656faadb2197744f780a5c2d0778fdc1c
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-07-24 15:44:13 +0300

    paytabs config

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/api/v1/payments-webhook", (req, res, next) => {
 
   let tranRef = req.body.tran_ref;
 
- paytabs.validatePayment(tranRef, async (response) => {
+  paytabs.validatePayment(tranRef, async (response) => {
     if (response.payment_result.response_status === "A") {
       // get cart depends on cartId
       const cart = await cartModel.findById(req.body.cart_id);
@@ -101,14 +101,12 @@ app.post("/api/v1/payments-webhook", (req, res, next) => {
         await cartModel.findByIdAndDelete(req.body.cart_id);
       }
 
-      return res.status(200).send({ status: "success", order });
+      res.status(200).json({ status: "success", order });
     } else {
-      return next(new ApiError("payment failed", 402));
+      res.status(402).json({ status: "payment failed" });
     }
   });
-      return res.status(200).json({ status: "success" });
-
-
+  res.status(200).json({ status: "success" });
 });
 
 app.all("*", (req, res, next) => {
