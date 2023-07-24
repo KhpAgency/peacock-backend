2cd228166424b00342725ae4894d55d2839cac52
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-07-24 16:47:31 +0300

    paytabs config

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -118,7 +118,6 @@ exports.createOnlinePaymentOrder = asyncHandler(async (req, res, next) => {
 
   let url = {
     callback: `${process.env.PAYTABS_CALLBACK_URL}`,
-    response: `https://webhook.site/92a3aee6-14f2-4b80-b8b9-c5c014a2e96f`,
   };
 
   let response_URLs = [url.callback];
@@ -160,53 +159,60 @@ exports.paymentWebhook = asyncHandler(async (req, res, next) => {
 
   let tranRef = req.body.tran_ref;
 
-  paytabs.validatePayment(tranRef, async (response) => {
-    if (response.payment_result.response_status !== "A") {
-      return next(
-        new ApiError(
-          `payment failed with status: ${response.payment_result.response_status}, reason: ${response.payment_result.response_message}`,
-          403
-        )
-      );
-    }
+  let queryRequested = function ($results){
+    console.log($results);
+}
 
-    // get cart depends on cartId
-    const cart = await cartModel.findById(req.body.cart_id);
+paytabs.validatePayment(tranRef, queryRequested);
 
-    // set order price depend on cart total price
-    const cartPrice = cart.totalCartPrice;
-    const totalorderPrice = cartPrice;
 
-    if (!cart) {
-      return next(
-        new ApiError(`No cart found for this id:${req.body.cart_id}`, 404)
-      );
-    }
+  // paytabs.validatePayment(tranRef, async (response) => {
+  //   if (response.payment_result.response_status !== "A") {
+  //     return next(
+  //       new ApiError(
+  //         `payment failed with status: ${response.payment_result.response_status}, reason: ${response.payment_result.response_message}`,
+  //         403
+  //       )
+  //     );
+  //   }
 
-    // create order with online payment method
-    const order = await orderModel.create({
-      user: cart.user,
-      orderNumber: `SA-4000${Math.floor(Math.random() * 1000000000)}`,
-      cartItems: cart.cartItems,
-      totalorderPrice,
-      shippingAddress: {
-        name: req.body.shipping_details.name,
-        details: req.body.shipping_details.street1,
-        city: req.body.shipping_details.city,
-        state: req.body.shipping_details.state,
-        phone: req.body.shipping_details.phone,
-      },
-      paymentMethod: "online payment",
-      isPaid: true,
-    });
+  //   // get cart depends on cartId
+  //   const cart = await cartModel.findById(req.body.cart_id);
 
-    if (order) {
-      // clear cart depending on cartId
-      await cartModel.findByIdAndDelete(req.body.cart_id);
-    }
+  //   // set order price depend on cart total price
+  //   const cartPrice = cart.totalCartPrice;
+  //   const totalorderPrice = cartPrice;
 
-    res.status(200).json({ message: "Success" });
-  });
+  //   if (!cart) {
+  //     return next(
+  //       new ApiError(`No cart found for this id:${req.body.cart_id}`, 404)
+  //     );
+  //   }
+
+  //   // create order with online payment method
+  //   const order = await orderModel.create({
+  //     user: cart.user,
+  //     orderNumber: `SA-4000${Math.floor(Math.random() * 1000000000)}`,
+  //     cartItems: cart.cartItems,
+  //     totalorderPrice,
+  //     shippingAddress: {
+  //       name: req.body.shipping_details.name,
+  //       details: req.body.shipping_details.street1,
+  //       city: req.body.shipping_details.city,
+  //       state: req.body.shipping_details.state,
+  //       phone: req.body.shipping_details.phone,
+  //     },
+  //     paymentMethod: "online payment",
+  //     isPaid: true,
+  //   });
+
+  //   if (order) {
+  //     // clear cart depending on cartId
+  //     await cartModel.findByIdAndDelete(req.body.cart_id);
+  //   }
+
+  //   res.status(200).json({ message: "Success" });
+  // });
 });
 
 exports.filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
