45ffe66dd22a95de17dfd42f1f91d75e3f52b0d0
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-07-24 17:15:39 +0300

    paytabs config

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -159,51 +159,53 @@ exports.paymentWebhook = asyncHandler(async (req, res, next) => {
 
   let tranRef = req.body.tran_ref;
 
-  if (req.body.payment_result.response_status !== "A") {
-    return next(
-      new ApiError(
-        `payment failed with status: ${req.body.payment_result.response_status}, reason: ${req.body.payment_result.response_message}`,
-        403
-      )
-    );
-  }
+  paytabs.validatePayment(tranRef, async (response) => {
+    if (response.payment_result.response_status !== "A") {
+      return next(
+        new ApiError(
+          `payment failed with status: ${response.payment_result.response_status}, reason: ${response.payment_result.response_message}`,
+          403
+        )
+      );
+    }
 
-  // get cart depends on cartId
-  const cart = await cartModel.findById(req.body.cart_id);
+    // get cart depends on cartId
+    const cart = await cartModel.findById(req.body.cart_id);
 
-  // set order price depend on cart total price
-  const cartPrice = cart.totalCartPrice;
-  const totalorderPrice = cartPrice;
+    // set order price depend on cart total price
+    const cartPrice = cart.totalCartPrice;
+    const totalorderPrice = cartPrice;
 
-  if (!cart) {
-    return next(
-      new ApiError(`No cart found for this id:${req.body.cart_id}`, 404)
-    );
-  }
+    if (!cart) {
+      return next(
+        new ApiError(`No cart found for this id:${req.body.cart_id}`, 404)
+      );
+    }
 
-  // create order with online payment method
-  const order = await orderModel.create({
-    user: cart.user,
-    orderNumber: `SA-4000${Math.floor(Math.random() * 1000000000)}`,
-    cartItems: cart.cartItems,
-    totalorderPrice,
-    shippingAddress: {
-      name: req.body.shipping_details.name,
-      details: req.body.shipping_details.street1,
-      city: req.body.shipping_details.city,
-      state: req.body.shipping_details.state,
-      phone: req.body.shipping_details.phone,
-    },
-    paymentMethod: "online payment",
-    isPaid: true,
+    // create order with online payment method
+    const order = await orderModel.create({
+      user: cart.user,
+      orderNumber: `SA-4000${Math.floor(Math.random() * 1000000000)}`,
+      cartItems: cart.cartItems,
+      totalorderPrice,
+      shippingAddress: {
+        name: req.body.shipping_details.name,
+        details: req.body.shipping_details.street1,
+        city: req.body.shipping_details.city,
+        state: req.body.shipping_details.state,
+        phone: req.body.shipping_details.phone,
+      },
+      paymentMethod: "online payment",
+      isPaid: true,
+    });
+
+    if (order) {
+      // clear cart depending on cartId
+      await cartModel.findByIdAndDelete(req.body.cart_id);
+    }
+
+    res.status(200).json({ message: "Success" });
   });
-
-  if (order) {
-    // clear cart depending on cartId
-    await cartModel.findByIdAndDelete(req.body.cart_id);
-  }
-
-  res.status(200).json({ message: "Success" });
 });
 
 exports.filterOrderForLoggedUser = asyncHandler(async (req, res, next) => {
