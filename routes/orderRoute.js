b5bded3634003a9c2a2cf4d00beb6f111bba03cf
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-07-26 17:00:47 +0300

    paytabs config

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -17,7 +17,7 @@ const Router = express.Router();
 const { protect, allowedTo } = require("../controllers/authController");
 
 Router.post("/payments-webhook", paymentWebhook);
-Router.post("/test", test);
+Router.get("/test", test);
 // Router.use(protect);
 
 Router.route("/:cartId").post(protect, allowedTo("user"), createCashOrder);
