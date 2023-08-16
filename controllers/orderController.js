7e6f309198f880d1c8b48d5c0f4a8ddb55f1087e
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-08-16 14:40:41 +0300

    sending order confirmation email

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -49,8 +49,15 @@ exports.createCashOrder = asyncHandler(async (req, res, next) => {
   let emailTamplate = `<!DOCTYPE html>
   <html lang="en">
     <head>
+
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+      integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+      crossorigin="anonymous"
+    />
   
-      <style>
+      <style type="text/css>
         @import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
         body {
           font-family: "Montserrat", sans-serif;
@@ -72,10 +79,7 @@ exports.createCashOrder = asyncHandler(async (req, res, next) => {
           color: #dedbdb;
         }
       </style>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"
-      />
+      
     </head>
     <body>
       <div class="row d-flex justify-content-center">
@@ -216,7 +220,6 @@ exports.createCashOrder = asyncHandler(async (req, res, next) => {
   `;
 
   try {
-    console.log(order);
     await sendEmail({
       email: req.user.email,
       subject: `${capitalizeFirlstLetterOfName}, Your order has been placed`,
