15ff699d332cb7323449fa6f49afe634c9c0aef1
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2023-08-21 13:22:09 +0300

    sending order confirmation emails

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ const path = require("path");
 const ApiError = require("./utils/ApiError");
 const globalError = require("./middlewares/errorMiddleware");
 const dbConnection = require("./config/database");
-const paytabs = require("paytabs_pt2");
-const moment = require('moment-timezone');
-moment.tz.setDefault('Africa/Cairo'); // Set the desired time zone to Cairo
 
 const userRoute = require("./routes/userRoute");
 const categoriesRoute = require("./routes/categoriesRoute");
--- controllers/orderController.js
+++ controllers/orderController.js
@@ -420,6 +420,8 @@ exports.paymentWebhook = asyncHandler(async (req, res, next) => {
     req.body.shipping_details.name.split(" ")[0].charAt(0).toUpperCase() +
     req.body.shipping_details.name.split(" ")[0].slice(1).toLocaleLowerCase();
 
+const date = `${JSON.stringify(order.createdAt).split(".")[0].split("T")[0]} ${JSON.stringify(order.createdAt).split(".")[0].split("T")[1]}`
+
     let emailTamplate = `
     <!DOCTYPE html>
   <html lang="en">
@@ -536,7 +538,7 @@ exports.paymentWebhook = asyncHandler(async (req, res, next) => {
   
         <div class="order-summary">
         <div style="display: flex; justify-content: right;">
-          <span style="text-align: right; color: #8e8e8e;">${JSON.stringify(order.createdAt).split(".")[0].split("T")[0]} ${JSON.stringify(order.createdAt).split(".")[0].split("T")[1]}"</span>
+          <span style="text-align: right; color: #8e8e8e;">${date.split('"').join('')}</span>
           </div>
           <h2>Order Placed</h2>
               <span class="font-weight d-block">Hello, ${capitalizeFirlstLetterOfName}</span>
